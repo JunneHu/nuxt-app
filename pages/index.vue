1e0736ac6ca6fe0b7a8af7c7ce28146fef3cf712
junney <1092622784@qq.com>
2020-08-10 18:27:57 +0800

    提交备份代码

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    1
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    const posts = await $axios.$get('https://it-console-mall.suuyuu.cn/addbanner');
+    console.log(posts)
+    return { posts };
+  },
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
